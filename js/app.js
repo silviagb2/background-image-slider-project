9e809c213c6aac48bb4809d1d74d8929d45d9464
silviagb <silviagb2@gmail.com>
2020-12-22 13:33:58 +0100

    finished+comentarios

--- js/app.js
+++ js/app.js
@@ -17,14 +17,16 @@
   
 function cambiarImagenLeft(){
     inicio--
+    //Cuando sea menor que 0, iremos a la última imágen del array
     if(inicio<0){
-      inicio = pictures.length -1
+      inicio = pictures.length -1 
     }
     container.style.backgroundImage = `url('./img/${pictures[inicio]}.jpeg')`;
 }
 
 function cambiarImagenRight(){
   inicio++
+  //Cuando lleguemos al último parámetro del length del array (que no existe) volveremos al inicio.
   if(inicio>pictures.length-1){
     inicio = 0
   }
