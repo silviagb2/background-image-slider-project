d616d5f16f9f1e7b54a8e8448acaecb3e79331bf
silviagb <silviagb2@gmail.com>
2020-12-22 13:24:57 +0100

    finished - no bonus

--- js/app.js
+++ js/app.js
@@ -5,3 +5,30 @@
     "contBcg-3",
     "contBcg-4"
   ];
+
+  let buttonRight = document.querySelector('.btn-right')
+  let buttonLeft = document.querySelector('.btn-left')
+  let container = document.querySelector ('.img-container')
+  let inicio = 0
+
+  buttonLeft.addEventListener('click',cambiarImagenLeft)
+
+  buttonRight.addEventListener('click',cambiarImagenRight)
+  
+function cambiarImagenLeft(){
+    inicio--
+    if(inicio<0){
+      inicio = pictures.length -1
+    }
+    container.style.backgroundImage = `url('./img/${pictures[inicio]}.jpeg')`;
+}
+
+function cambiarImagenRight(){
+  inicio++
+  if(inicio>pictures.length-1){
+    inicio = 0
+  }
+  container.style.backgroundImage = `url('./img/${pictures[inicio]}.jpeg')`;
+}
+
+
